47f5a86c7c0d9acceafec87d4dcfc14dafd1d657
cgillam0513 <93233682+cgillam0513@users.noreply.github.com>
2021-11-01 09:32:41 -0400

    Commit updated tests
    Commit updated tests

--- tests/test_math_trees.py
+++ tests/test_math_trees.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+# Pearson Coding Challenge Tests
+# Author: Christopher Gillam
+# Date: 10/01/2021
+
 import unittest
 
-from math_trees.math_trees import parse_str, eval_expr
+from math_trees.math_trees import parse_str
 
 
 class TestMathTrees(unittest.TestCase):
@@ -47,3 +51,63 @@ class TestMathTrees(unittest.TestCase):
         math_tree = parse_str(math_string)
         value = math_tree.eval()
         self.assertEqual(value, 15.0)
+
+    def test8(self):
+        math_string = "3*+4"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, 12)
+
+    def test9(self):
+        math_string = "3*-4"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, -12)
+
+    def test10(self):
+        math_string = ".3+.5"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, 0.8)
+
+    def test11(self):
+        math_string = "-.3+-0.5"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, -0.8)
+
+    def test12(self):
+        math_string = "0.3+0.5"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, 0.8)
+
+    def test13(self):
+        math_string = "-0.3+-0.5"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, -0.8)
+
+    def test14(self):
+        math_string = "+0.3++0.5"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, 0.8)
+
+    def test15(self):
+        math_string = "+.3++.5"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, 0.8)
+
+    def test16(self):
+        math_string = "00000123"
+        math_tree = parse_str(math_string)
+        value = math_tree.eval()
+        self.assertEqual(value, 123)
+
+    def test17(self):
+        math_string = "3(2+(4-6)-3)*9+5"
+        math_tree = parse_str(math_string)
+        s = str(math_tree)
+        self.assertEqual(s, '3*(2+(4-6)-3)*9+5')
